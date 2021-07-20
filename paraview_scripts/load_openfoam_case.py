a2800697639bef0fbf11ad4cdd7166ec12498d9b
Ricardo Franco <ricardofranco95e@gmail.com>
2021-07-20 18:12:25 -0500

    changed cell_arrays to module constant

--- paraview_scripts/load_openfoam_case.py
+++ paraview_scripts/load_openfoam_case.py
@@ -2,30 +2,35 @@
 
 #### import the simple module from the paraview
 from paraview.simple import *
+import json
 
 #### disable automatic camera reset on 'Show'
 paraview.simple._DisableFirstRenderCameraReset()
 
+LES_DEFAULT_ARRAYS = ('DESRegionMean',
+                      'DESRegionPrime2Mean',
+                      'UMean',
+                      'UPrime2Mean',
+                      'kMean',
+                      'nutMean',
+                      'pMean',
+                      'pPrime2Mean',
+                      'wallShearStressMean',
+                      'yPlusMean')
 
-def get_openfoam_source(case_dir):
+
+def get_openfoam_source(case_dir, cell_arrays=LES_DEFAULT_ARRAYS):
     """Return a ParaView source of an OpenFOAM case.
 
      Args:
          case_dir (str): Path to OpenFOAM case.
-         cell_arrays (list): Names of the cellArrays to load."""
+         cell_arrays (list): Names of the cellArrays to load.
+         """
     # create a new 'OpenFOAMReader'
     file_name = "{case_dir}/system/controlDict".format(case_dir=case_dir)
     case_source = OpenFOAMReader(registrationName='controlDict',
                                  FileName=file_name)
     case_source.MeshRegions = ['internalMesh']
-    case_source.CellArrays = ['DESRegionMean',
-                              'DESRegionPrime2Mean',
-                              'UMean',
-                              'UPrime2Mean',
-                              'kMean',
-                              'nutMean',
-                              'pMean',
-                              'pPrime2Mean',
-                              'wallShearStressMean',
-                              'yPlusMean']
+    case_source.CellArrays = cell_arrays
     return case_source
+
